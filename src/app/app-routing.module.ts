e185b8b7a06b427d1c13eefc142dfeda77bf54d0
Andrés Alejandro Cortés Poveda <andres.alejandro.cortes@correounivalle.edu.co>
2023-12-03 01:39:10 -0500

    added visual change in rate parameter, added move to top movement on page changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { MyListComponent } from './my-list/my-list.component';
 
 const routes: Routes = [
   { path:"", component: HomeComponent},
-  { path:"movieDetails/:id", component: MovieDetailsComponent},
+  { path:"movieDetails/:id", component: MovieDetailsComponent,},
   { path:"myList", component: MyListComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
